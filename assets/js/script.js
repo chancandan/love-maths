b9f9e989392f1c5800ee5652f90a82ce28c7bfde
chancandan <daniellyons@live.ie>
2023-02-13 15:09:28 +0000

    Setting up event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function {
+    
+})
+
 function runGame() {
 
 }
